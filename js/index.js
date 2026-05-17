bbf0481a7a876bd8ae72579cd22040113136e83d
Mauricio <mauriciodesena331@gmail.com>
2026-05-17 14:41:29 -0300

    commit inicial

---
+++ js/index.js
@@ -0,0 +1,25 @@
+//MAIN
+function ola(){
+    alert("Mauricio mandou um beijo! MUAC😘");
+    window.location.href = "file:///D:/sitenatalyyy/surpresanaty.html"
+}
+//SURPRESA
+function alerta(){
+    alert("SITE PARA GUARDAR ALGUMAS MEMORIAS TALVEZ 😀")
+}
+//INTRO
+function butao(){
+const inputS = document.getElementById('senha');
+const valorS = inputS.value;
+const input = document.getElementById('nome');
+const valor = input.value;
+if(valor ==  "Nataly Melo" && valorS == "natalinda"){
+    window.location.href = "file:///D:/sitenatalyyy/main.html"
+}else{
+    alert("TA DE SACANAGEM TU NÃO É A NATALY")
+}
+}
+// FUNÇÃO SEGREDO
+function secreto(){
+    window.location.href="file:///D:/sitenatalyyy/carta.html"
+}
